0c6caed5a78d294195eece3023cf11267c8532ad
IDGitHubMan <40811090+IDGitHubMan@users.noreply.github.com>
2021-06-15 09:46:46 -0400

    Stuff again

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -7,33 +7,31 @@ public class GameManagerScript : MonoBehaviour
 {
     //HUD
     //Health + Rage (mana) bars + labels
-    public Slider healthBar;
-    public Text healthLabel;
-    public Slider rageBar;
-    public Text rageLabel;
-    //Map + Quests window
-    public RenderTexture map;
-    public GameObject quests;
-    public Button pause;
-    public Button resume;
-    public Button main;
+    // public Slider healthBar;
+    // public Text healthLabel;
+    // public Slider rageBar;
+    // public Text rageLabel;
+    // //Map + Quests window
+    // public RenderTexture map;
+    // public GameObject quests;
+    // public Button pause;
+    // public Button resume;
+    // public Button main;
 
-    [Header("System Rules and Numbers")]
-    [Tooltip("Force of Gravity (Deefault -9.81)")] public float gravityForce = -9.81f;
-    [Tooltip("Max Wander Distance for NPCs")] public float wanderLimit = -30.0f;
-    [Tooltip("NPC Hearing Range")] public float hearingRange = 10.0f;
-    [Tooltip("NPC Panic Chance")] public float panicChance = 0.5f;
+    // [Header("System Rules and Numbers")]
+    // [Tooltip("Force of Gravity (Deefault -9.81)")] public float gravityForce = -9.81f;
+    // [Tooltip("Max Wander Distance for NPCs")] public float wanderLimit = -30.0f;
+    // [Tooltip("NPC Hearing Range")] public float hearingRange = 10.0f;
+    // [Tooltip("NPC Panic Chance")] public float panicChance = 0.5f;
 
-    [Header("Player Stats")]
-    [Tooltip("Player Movement Speed")] public float playerSpeed = 2.0f;
-    [Tooltip("Base Player Health")] public int playerHealth = 50;
-    [Tooltip("Player Rage")] public int rageBase = 25;
-    public GameObject player;
+    // [Header("Player Stats")]
+    // [Tooltip("Player Movement Speed")] public float playerSpeed = 2.0f;
+    // [Tooltip("Base Player Health")] public int playerHealth = 50;
+    // [Tooltip("Player Rage")] public int rageBase = 25;
+    // public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        healthBar.maxValue = playerHealth;
-        rageBar.maxValue = rageBase;
     }
 
     // Update is called once per frame
--- Assets/Scripts/SlasherScript.cs
+++ Assets/Scripts/SlasherScript.cs
@@ -4,31 +4,15 @@ using UnityEngine;
 
 public class SlasherScript : MonoBehaviour
 {
-    enum behavioralState{Idle,Confused,Chasing,Panicking,Possessed}
-    public float moveSpeed = 2.0f;
-    public bool colliding;
-    public float x;
-    public GameObject homePoint;
-    public GameObject player;
-    public Vector2 loc;
-    public Vector2 playerLoc;
-    public Vector2 selfLoc;
-    public BoxCollider2D collider;
-
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
-        collider = gameObject.GetComponent<BoxCollider2D>();
-        if (!collider){
-            collider = gameObject.AddComponent<BoxCollider2D>();
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerLoc = new Vector2(player.GetComponent<Transform>().position.x, player.GetComponent<Transform>().position.z);
-        selfLoc = new Vector2(this.GetComponent<Transform>().position.x, this.GetComponent<Transform>().position.z);
+        
     }
 }
