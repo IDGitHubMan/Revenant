b93d3db38e8fbff7b8bd2a39a39fd5c6c11d7fa2
IDGitHubMan <40811090+IDGitHubMan@users.noreply.github.com>
2021-06-17 21:54:56 -0400

    Test

--- Assets/Scripts/BarrelScript.cs
+++ Assets/Scripts/BarrelScript.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class BarrelScript : MonoBehaviour
 {
+    public bool isCover;
+    public bool isMobile;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
     }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float speed;
+    public float jump;
+    Rigidbody2D rb;
+    public bool canClimb;
+    public bool lowCover;
+    public bool standCover;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent <Rigidbody2D>();
+        speed = 7.5f;
     }
+    
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector3 (x*speed,rb.velocity.y,0);
     }
 }
