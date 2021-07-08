3440d7634bb4cb9c72ba3d0f99b20b4e66fb782f
IDGitHubMan <40811090+IDGitHubMan@users.noreply.github.com>
2021-07-07 20:04:27 -0400

    Still more testing.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject health;
+    public GameObject skillBar;
+    public int hitCount = 6;
+    public float rage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FloorBuilder(10);
+    }
+
+    public void RoomBuilder(bool slasher, bool pistolman, bool shotgunner, bool demoman, int size, string setting){
+        Debug.Log("Generating room...");
+    }
+
+    public void FloorBuilder(int size){
+        int length = 0;
+        Debug.Log("Building floor...");
+        while (length<=size){
+            bool slashers = Random.value > 0.5f;
+            bool pistolmen = Random.value > 0.5f;
+            bool shotgunners = Random.value > 0.5f;
+            bool demomen = Random.value > 0.5f;
+            int roomSize = Random.Range(1,size+1);
+            RoomBuilder(slashers,pistolmen,shotgunners,demomen,size,"theater");
+            length += roomSize;
+            size -= roomSize;
+            Debug.Log(size);
+        }
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public float speed;
-    public float jump;
     Rigidbody2D rb;
     public bool canClimb;
+    public bool isClibming;
+    public bool canLowCover;
     public bool lowCover;
+    public bool canStandCover;
     public bool standCover;
+    public bool canGrab;
+    public bool canMelee;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent <Rigidbody2D>();
         speed = 7.5f;
+        canClimb = false;
+        canLowCover = false;
+        canStandCover = false;
+        canGrab = false;
+        canMelee = false;
     }
     
 
@@ -23,5 +32,29 @@ public class PlayerScript : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         rb.velocity = new Vector3 (x*speed,rb.velocity.y,0);
+        if (canClimb){
+            float y = Input.GetAxis("Vertical");
+            rb.velocity = new Vector3 (x*speed,y*speed,0);
+        }
+        rb.velocity = new Vector3(rb.velocity.x,rb.velocity.y,0);
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        if (col.collider.name=="expBarrel" || col.collider.name=="barrel"){
+            canLowCover = true;
+            canMelee = true;
+        }
+
+        else if (col.collider.name=="ladder"){
+            canClimb = true;
+        }
+        else if (col.collider.name=="slasher" || col.collider.name=="pistol"||col.collider.name=="shotgun"){
+            canGrab = true;
+            canMelee = true;
+        }
+        else if (col.collider.name=="doorway"){}
+        else{
+            canClimb = false;
+        }
     }
 }
